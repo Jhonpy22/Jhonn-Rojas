1a20fe2e936d8c26bcf5be83151953444133c7e6
Jhonpy22 <jhonngranadosrojas@gmail.com>
2023-06-02 08:05:14 -0600

    hola

---
+++ Jhonn.js
@@ -0,0 +1,58 @@
+/*var x= 10;
+if (x < 5) {
+    console.log ("x es menor que 5");
+}else if ( x < 10){
+    console.log ("x es menor que 10 pero mayor o igual a 5");
+
+}else{
+    console.log ("x es mayor o igual a 10");
+}*/
+
+
+
+
+
+var lecciones = "dias libres";
+var mensaje;
+
+switch (lecciones){
+    case "dias libres":
+     mensaje = "lunes y jueves no tengo clases";
+      break;
+    case "miercoles":
+     mensaje = " miercoles tengo clases";
+      break; 
+    case "viernes":
+     mensaje = " viernes tengo clases";
+     break;
+     default:
+      mensaje = "tengo dias libres";
+
+}
+console.log (mensaje);     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*for ( var i = 0; i < 7 ; i ++) {
+    console. log ( "trabajar toda la semana") + (i + 1);
+}*/
+
+/*var contador = 0;
+while ( contador < 5 ) {
+  console.log ( "iteración numero" + (contador + 1 ));
+   contador++;  
+}*/
